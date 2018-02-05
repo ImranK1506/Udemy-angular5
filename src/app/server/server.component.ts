1891c7811c3b6798cdf2187fe3fcd2e23d91501a
Imran Khan <imran.khan@kpn.com>
2018-02-05 16:14:23 +0100

    dynamic styling

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,12 +9,18 @@ export class ServerComponent implements OnInit {
   serverID: number = 10;
   serverStatus: string = 'offline';
 
-  constructor() { }
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   ngOnInit() {
   }
 
   getServerStatus() {
   return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
